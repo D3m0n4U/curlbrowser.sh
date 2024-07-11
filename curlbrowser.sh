c29584fe536f1f3e424f4b680fdd16334eb03925
D3m0n4U <160055791+D3m0n4U@users.noreply.github.com>
2024-07-11 11:25:24 +0530

    Update curlbrowser.sh

--- curlbrowser.sh
+++ curlbrowser.sh
@@ -5,7 +5,7 @@ echo "Enter a URL: "
 read url
 
 # Use curl to fetch the URL
-html=$(curl -s "$url")
+html=$(curl -s -k "$url")
 
 # Open the HTML in the default web browser
 echo "$html" | w3m -dump -T text/html
